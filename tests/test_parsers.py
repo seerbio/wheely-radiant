63b549fe87a74ac7eb819fe0273bea069486c039
Seth Just <sjust@seer.bio>
2023-05-30 14:23:53 -0700

    Update column annotations for Parquet format

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -9,30 +9,39 @@ def test_read_pythia_features(spark_session, pythia_features):
     psms = read_pythia_features(pythia_features, spark_session)
     assert isinstance(psms.data, pyspark.sql.DataFrame)
     assert psms.data.count() == 1000
-    assert list(psms.spectrum_columns) == ["filename", "scanNumber"]  # TODO
+    assert list(psms.spectrum_columns) == ["filename", "scanNumber"]
     assert all(col in psms.spectra.columns for col in psms.spectrum_columns)
 
-    # TODO
     scores = {
-        "cosine_similarity",
-        "klDivergence",
-        "Score",
-        "hyperscore",
-        "deltaScore",
-        "meanErrorPPM",
-        "meanAbsoluteErrorPPM",
-        "leftOverRawScanIntensity",
-        "extractedIonCount",
-        "aCount",
-        "bCount",
-        "yCount",
-        "b2Count",
-        "y2Count",
-        "yNH3Count",
-        "yH2OCount",
-        "bNH3Count",
-        "bH2OCount",
-        "scanRank",
+        "charge",
+        "cosineSim",
+        "discScore",
+        "discScoreMax",
+        "discScoreMean",
+        "discScoreMedian",
+        "discScoreMin",
+        "discScoreStDev",
+        "fractionFound",
+        "frameCandidateCount",
+        "frameError",
+        "frameFStat",
+        "frameRankDiscScore",
+        "frameRankScore",
+        "ionsFound",
+        "isotopeFoundCount",
+        "klDiv",
+        "missedCleavages",
+        "monoIsoOffset",
+        "ms1CosineSim",
+        "mz",
+        "mzFound",
+        "peptideSize",
+        "ppmDiffMs1",
+        "score",
+        "scoreMax",
+        "scoreMean",
+        "scoreMedian",
+        "scoreStDev",
     }
     assert set(psms.score_columns) == scores
 
@@ -41,8 +50,8 @@ def test_read_pythia_features(spark_session, pythia_features):
     target_df = psms.data.select(psms.targets).toPandas()
 
     assert target_df.shape == (1000, 1)
-    assert target_df[target_df.columns[0]].sum() == 691  # TODO
-    assert (~target_df[target_df.columns[0]]).sum() == 1000 - 691  # TODO
+    assert target_df[target_df.columns[0]].sum() == 481
+    assert (~target_df[target_df.columns[0]]).sum() == 1000 - 481
 
 
 def test_read_pythia_hdf_features(spark_session, pythia_hdf_features):
