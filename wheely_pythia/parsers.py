c93eaaecf543347405bc3384bd43cf40c773371b
Seth Just <sjust@seer.bio>
2023-12-12 14:36:54 -0800

    Update tests; precursor-centric semantics should only apply to DIA

--- wheely_pythia/parsers.py
+++ wheely_pythia/parsers.py
@@ -85,7 +85,7 @@ def read_pythia_features(
     return _PsmDataset(
         psms_df,
         target_column="target",
-        spectrum_columns=["filename", "precursor", "scanNumber"],
+        spectrum_columns=["filename", "scanNumber"],
         score_columns=[
             "cosine_similarity",
             "klDivergence",
@@ -152,7 +152,7 @@ def read_pythia_parquet(
     return _PsmDataset(
         psms_df,
         target_column="target",
-        spectrum_columns=["filename", "scanNumber"],
+        spectrum_columns=["filename", "precursor", "scanNumber"],
         score_columns=[
             "b2Corr",
             "b2b3CosineSimSum",
